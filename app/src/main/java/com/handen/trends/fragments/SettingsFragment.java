ebfd3dfb05c4188377fb0cac0d4b1c30b72cf1a3
Rabochya305 <ivanhanden@gmail.com>
2018-02-12 14:29:57 +0300

    Добавлены и найдены View в SettingsFragment

--- app/src/main/java/com/handen/trends/fragments/SettingsFragment.java
+++ app/src/main/java/com/handen/trends/fragments/SettingsFragment.java
@@ -1,19 +1,20 @@
 package com.handen.trends.fragments;
 
 
+import android.opengl.ETC1;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.handen.trends.R;
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class SettingsFragment extends Fragment {
 
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private EditText nicknameEditText;
 
     public SettingsFragment() {
         // Required empty public constructor
@@ -23,8 +24,14 @@ public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        emailEditText = (EditText) view.findViewById(R.id.email_edit_text);
+        passwordEditText = (EditText) view.findViewById(R.id.password_edit_text);
+        nicknameEditText = (EditText) view.findViewById(R.id.nickname_edit_text);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+
+        return view;
     }
 
 }
